76352469505fa6adb8abcf02f29856659043f945
BrainstormTrooper <brainstormtrooper@free.fr>
2012-10-03 08:50:54 +0200

    file with useful parts...

---
+++ cron_backup_2.php
@@ -0,0 +1,120 @@
+<?php
+function Zip($source, $destination)
+{
+    if (!extension_loaded('zip') || !file_exists($source)) {
+        return false;
+    }
+
+    $zip = new ZipArchive();
+    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+        return false;
+    }
+
+    $source = str_replace('\\', '/', realpath($source));
+
+    if (is_dir($source) === true)
+    {
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+        foreach ($files as $file)
+        {
+            $file = str_replace('\\', '/', $file);
+
+            // Ignore "." and ".." folders
+            if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+                continue;
+
+            $file = realpath($file);
+
+            if (is_dir($file) === true)
+            {
+                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+            }
+            else if (is_file($file) === true)
+            {
+                $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            }
+        }
+    }
+    else if (is_file($source) === true)
+    {
+        $zip->addFromString(basename($source), file_get_contents($source));
+    }
+
+
+$dbhost   = "mysql10**.servage.net";
+$dbuser   = "USERNAME";
+$dbpwd    = "PASSWORD";
+$dbname   = "DATABASENAME";
+$dumpfile = $dbname . "_" . date("Y-m-d_H-i-s") . ".sql";
+
+passthru("/usr/bin/mysqldump --opt --host=$dbhost --user=$dbuser --password=$dbpwd $dbname > $dumpfile");
+
+// report - disable with // if not needed
+// must look like "-- Dump completed on ..." 
+
+echo "$dumpfile "; passthru("tail -1 $dumpfile");
+
+// or...
+
+backup_tables('localhost','username','password','blog');
+
+
+/* backup the db OR just a table */
+function backup_tables($host,$user,$pass,$name,$tables = '*')
+{
+	
+	$link = mysql_connect($host,$user,$pass);
+	mysql_select_db($name,$link);
+	
+	//get all of the tables
+	if($tables == '*')
+	{
+		$tables = array();
+		$result = mysql_query('SHOW TABLES');
+		while($row = mysql_fetch_row($result))
+		{
+			$tables[] = $row[0];
+		}
+	}
+	else
+	{
+		$tables = is_array($tables) ? $tables : explode(',',$tables);
+	}
+	
+	//cycle through
+	foreach($tables as $table)
+	{
+		$result = mysql_query('SELECT * FROM '.$table);
+		$num_fields = mysql_num_fields($result);
+		
+		$return.= 'DROP TABLE '.$table.';';
+		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$return.= "\n\n".$row2[1].";\n\n";
+		
+		for ($i = 0; $i < $num_fields; $i++) 
+		{
+			while($row = mysql_fetch_row($result))
+			{
+				$return.= 'INSERT INTO '.$table.' VALUES(';
+				for($j=0; $j<$num_fields; $j++) 
+				{
+					$row[$j] = addslashes($row[$j]);
+					$row[$j] = ereg_replace("\n","\\n",$row[$j]);
+					if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+					if ($j<($num_fields-1)) { $return.= ','; }
+				}
+				$return.= ");\n";
+			}
+		}
+		$return.="\n\n\n";
+	}
+	
+	//save file
+	$handle = fopen('db-backup-'.time().'-'.(md5(implode(',',$tables))).'.sql','w+');
+	fwrite($handle,$return);
+	fclose($handle);
+}
+
+
+?>
